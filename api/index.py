a85bffe55700bead67c61d6da5eb40bca4c0f4f7
yashm277 <f20210001@pilani.bits-pilani.ac.in>
2024-04-01 17:58:48 +0530

    get test

--- api/index.py
+++ api/index.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-@app.route('/about')
-def about():
-    return 'About'
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
