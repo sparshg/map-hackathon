68494682b573b030e9a887187c4b8ae122cbbfc5
Yash Mundada <F20210001@pilani.bits-pilani.ac.in>
2024-04-06 22:58:09 +0000

    changed get req

--- api/index.py
+++ api/index.py
@@ -40,86 +40,86 @@ class HelloWorld(Resource):
 import requests
 
 class Pothole(Resource):
-    def get(self):
-        try:
-            data = request.data.decode('utf-8')
-            data_io = StringIO(data)
-            columns_for_get = ['latitude', 'longitude']  
-            df = pd.read_csv(data_io, names=columns_for_get)
-            nearest_points_all = []
-            if df.shape[0] == 1:
-                return_message = "Points"
-                for index, row in df.iterrows():
-                    lat, lon = row['latitude'], row['longitude']
+    # def get(self):
+    #     try:
+    #         data = request.data.decode('utf-8')
+    #         data_io = StringIO(data)
+    #         columns_for_get = ['latitude', 'longitude']  
+    #         df = pd.read_csv(data_io, names=columns_for_get)
+    #         nearest_points_all = []
+    #         if df.shape[0] == 1:
+    #             return_message = "Points"
+    #             for index, row in df.iterrows():
+    #                 lat, lon = row['latitude'], row['longitude']
 
-                    lat_long_df = pd.read_csv('all_pothole_data.csv')
-                    lat_long_df.columns = ['Latitude', 'Longitude', 'Prediction']  
-                    lat_long_df['distance'] = lat_long_df.apply(lambda row: haversine(lat, lon, row['Latitude'], row['Longitude']), axis=1)        
-                    nearest_points = lat_long_df[(lat_long_df['distance'] <= 5) & (lat_long_df['Prediction'] > 0.1)]
+    #                 lat_long_df = pd.read_csv('all_pothole_data.csv')
+    #                 lat_long_df.columns = ['Latitude', 'Longitude', 'Prediction']  
+    #                 lat_long_df['distance'] = lat_long_df.apply(lambda row: haversine(lat, lon, row['Latitude'], row['Longitude']), axis=1)        
+    #                 nearest_points = lat_long_df[(lat_long_df['distance'] <= 5) & (lat_long_df['Prediction'] > 0.1)]
 
-                    if not nearest_points.empty:
-                        nearest_points = nearest_points.sort_values(by='distance')
-                        nearest_points_list = nearest_points.to_dict('records')
+    #                 if not nearest_points.empty:
+    #                     nearest_points = nearest_points.sort_values(by='distance')
+    #                     nearest_points_list = nearest_points.to_dict('records')
 
-                        # Perform reverse geocoding for each nearest point
-                        for point in nearest_points_list:
-                            reverse_geocode_result = self.reverse_geocode(point['Latitude'], point['Longitude'])
-                            point['reverse_geocode'] = reverse_geocode_result
+    #                     # Perform reverse geocoding for each nearest point
+    #                     # for point in nearest_points_list:
+    #                     #     reverse_geocode_result = self.reverse_geocode(point['Latitude'], point['Longitude'])
+    #                     #     point['reverse_geocode'] = reverse_geocode_result
 
-                        nearest_points_all.extend(nearest_points_list)
+    #                     nearest_points_all.extend(nearest_points_list)
 
-                if nearest_points_all:
-                    return {return_message: nearest_points_all}
-                else:
-                    print("No points within 5m radius")
-                    return {'message': 'No points within 5m radius'}
-            else:
-                return_message = "Points"
-                # want to get previous row data as well as current row data, from second row 
-                for index, row in df.iterrows():
-                    if index == 0:
-                        continue
-                    lat1, lon1 = df.iloc[index-1]['latitude'], df.iloc[index-1]['longitude']
-                    lat2, lon2 = row['latitude'], row['longitude']
-                    lat_long_df = pd.read_csv('all_pothole_data.csv')
-                    lat_long_df.columns = ['Latitude', 'Longitude', 'Prediction']  
-                    lat_long_df['distance'] = lat_long_df.apply(lambda row: is_point_on_segment((row['Latitude'], row['Longitude']), (lat1, lon1), (lat2, lon2), tolerance=5), axis=1)
-                    nearest_points = lat_long_df[(lat_long_df['distance'] == True) & (lat_long_df['Prediction'] > 0.1)]
-                    if not nearest_points.empty:
-                        nearest_points = nearest_points.sort_values(by='distance')
-                        nearest_points_list = nearest_points.to_dict('records')
+    #             if nearest_points_all:
+    #                 return {return_message: nearest_points_all}
+    #             else:
+    #                 print("No points within 5m radius")
+    #                 return {'message': 'No points within 5m radius'}
+    #         else:
+    #             return_message = "Points"
+    #             # want to get previous row data as well as current row data, from second row 
+    #             for index, row in df.iterrows():
+    #                 if index == 0:
+    #                     continue
+    #                 lat1, lon1 = df.iloc[index-1]['latitude'], df.iloc[index-1]['longitude']
+    #                 lat2, lon2 = row['latitude'], row['longitude']
+    #                 lat_long_df = pd.read_csv('all_pothole_data.csv')
+    #                 lat_long_df.columns = ['Latitude', 'Longitude', 'Prediction']  
+    #                 lat_long_df['distance'] = lat_long_df.apply(lambda row: is_point_on_segment((row['Latitude'], row['Longitude']), (lat1, lon1), (lat2, lon2), tolerance=5), axis=1)
+    #                 nearest_points = lat_long_df[(lat_long_df['distance'] == True) & (lat_long_df['Prediction'] > 0.1)]
+    #                 if not nearest_points.empty:
+    #                     nearest_points = nearest_points.sort_values(by='distance')
+    #                     nearest_points_list = nearest_points.to_dict('records')
 
-                        # Perform reverse geocoding for each nearest point
-                        for point in nearest_points_list:
-                            reverse_geocode_result = self.reverse_geocode(point['Latitude'], point['Longitude'])
-                            point['reverse_geocode'] = reverse_geocode_result
+    #                     # Perform reverse geocoding for each nearest point
+    #                     # for point in nearest_points_list:
+    #                     #     reverse_geocode_result = self.reverse_geocode(point['Latitude'], point['Longitude'])
+    #                     #     point['reverse_geocode'] = reverse_geocode_result
 
-                        nearest_points_all.extend(nearest_points_list)
+    #                     nearest_points_all.extend(nearest_points_list)
 
-                if nearest_points_all:
-                    return {return_message: nearest_points_all}
-                else:
-                    print("No points within 5m radius of the line segment provided.")
-                    return {'message': 'No points within 5m radius of line segment provided'} 
-        except Exception as e:
-            return {'error': str(e)}
+    #             if nearest_points_all:
+    #                 return {return_message: nearest_points_all}
+    #             else:
+    #                 print("No points within 5m radius of the line segment provided.")
+    #                 return {'message': 'No points within 5m radius of line segment provided'} 
+    #     except Exception as e:
+    #         return {'error': str(e)}
     
-    def reverse_geocode(self, latitude, longitude):
-        REST_KEY = "490a70fb32ef272d74dc59c0bf7b731e"  
-        base_url = "https://apis.mappls.com/advancedmaps/v1"
-        url = f"{base_url}/{REST_KEY}/rev_geocode"
-        params = {
-            "lat": latitude,
-            "lng": longitude
-        }
-        headers = {
-            "Content-Type": "application/json"
-        }
-        response = requests.get(url, params=params, headers=headers)
-        if response.status_code == 200:
-            return response.json()  
-        else:
-            return None  
+    # def reverse_geocode(self, latitude, longitude):
+    #     REST_KEY = "490a70fb32ef272d74dc59c0bf7b731e"  
+    #     base_url = "https://apis.mappls.com/advancedmaps/v1"
+    #     url = f"{base_url}/{REST_KEY}/rev_geocode"
+    #     params = {
+    #         "lat": latitude,
+    #         "lng": longitude
+    #     }
+    #     headers = {
+    #         "Content-Type": "application/json"
+    #     }
+    #     response = requests.get(url, params=params, headers=headers)
+    #     if response.status_code == 200:
+    #         return response.json()  
+    #     else:
+    #         return None  
 
     
 
@@ -178,10 +178,91 @@ class Pothole(Resource):
 
 
 
+class Pothole2(Resource):
+    def post(self):
+        try:
+            data = request.data.decode('utf-8')
+            data_io = StringIO(data)
+            columns_for_get = ['latitude', 'longitude']  
+            df = pd.read_csv(data_io, names=columns_for_get)
+            nearest_points_all = []
+            if df.shape[0] == 1:
+                return_message = "Points"
+                for index, row in df.iterrows():
+                    lat, lon = row['latitude'], row['longitude']
 
+                    lat_long_df = pd.read_csv('all_pothole_data.csv')
+                    lat_long_df.columns = ['Latitude', 'Longitude', 'Prediction']  
+                    lat_long_df['distance'] = lat_long_df.apply(lambda row: haversine(lat, lon, row['Latitude'], row['Longitude']), axis=1)        
+                    nearest_points = lat_long_df[(lat_long_df['distance'] <= 5) & (lat_long_df['Prediction'] > 0.1)]
+
+                    if not nearest_points.empty:
+                        nearest_points = nearest_points.sort_values(by='distance')
+                        nearest_points_list = nearest_points.to_dict('records')
+
+                        # Perform reverse geocoding for each nearest point
+                        # for point in nearest_points_list:
+                        #     reverse_geocode_result = self.reverse_geocode(point['Latitude'], point['Longitude'])
+                        #     point['reverse_geocode'] = reverse_geocode_result
+
+                        nearest_points_all.extend(nearest_points_list)
+
+                if nearest_points_all:
+                    return {return_message: nearest_points_all}
+                else:
+                    print("No points within 5m radius")
+                    return {'message': 'No points within 5m radius'}
+            else:
+                return_message = "Points"
+                # want to get previous row data as well as current row data, from second row 
+                for index, row in df.iterrows():
+                    if index == 0:
+                        continue
+                    lat1, lon1 = df.iloc[index-1]['latitude'], df.iloc[index-1]['longitude']
+                    lat2, lon2 = row['latitude'], row['longitude']
+                    lat_long_df = pd.read_csv('all_pothole_data.csv')
+                    lat_long_df.columns = ['Latitude', 'Longitude', 'Prediction']  
+                    lat_long_df['distance'] = lat_long_df.apply(lambda row: is_point_on_segment((row['Latitude'], row['Longitude']), (lat1, lon1), (lat2, lon2), tolerance=5), axis=1)
+                    nearest_points = lat_long_df[(lat_long_df['distance'] == True) & (lat_long_df['Prediction'] > 0.1)]
+                    if not nearest_points.empty:
+                        nearest_points = nearest_points.sort_values(by='distance')
+                        nearest_points_list = nearest_points.to_dict('records')
+
+                        # Perform reverse geocoding for each nearest point
+                        # for point in nearest_points_list:
+                        #     reverse_geocode_result = self.reverse_geocode(point['Latitude'], point['Longitude'])
+                        #     point['reverse_geocode'] = reverse_geocode_result
+
+                        nearest_points_all.extend(nearest_points_list)
+
+                if nearest_points_all:
+                    return {return_message: nearest_points_all}
+                else:
+                    print("No points within 5m radius of the line segment provided.")
+                    return {'message': 'No points within 5m radius of line segment provided'} 
+        except Exception as e:
+            return {'error': str(e)}
+    
+    def reverse_geocode(self, latitude, longitude):
+        REST_KEY = "490a70fb32ef272d74dc59c0bf7b731e"  
+        base_url = "https://apis.mappls.com/advancedmaps/v1"
+        url = f"{base_url}/{REST_KEY}/rev_geocode"
+        params = {
+            "lat": latitude,
+            "lng": longitude
+        }
+        headers = {
+            "Content-Type": "application/json"
+        }
+        response = requests.get(url, params=params, headers=headers)
+        if response.status_code == 200:
+            return response.json()  
+        else:
+            return None  
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(Pothole, '/potholes')
+api.add_resource(Pothole2, '/getPotholes')
 
 if __name__ == '__main__':
     app.run(debug=True)
